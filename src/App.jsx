7cfaa2afa8ad0bff44417d200dc63052873df1b4
Sampriti Mondal <sampriti400@gmail.com>
2026-05-17 01:14:07 +0530

    Updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import SearchView from "./components/SearchView";
 import CuisineBar from "./components/Cuisine";
 import HomeView from "./components/HomeView";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { RECIPE_STYLES } from "./components/RecipeStyles";
+import { RECIPE_STYLES } from "./components/Recipestyles";
 
 const API_URL = "https://www.themealdb.com/api/json/v1/1/";
 
